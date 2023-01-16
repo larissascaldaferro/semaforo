4d63db399fada996ce6f0d5461c82c8bc3098969
Larissa Scaldaferro Amichi <111795955+larissascaldaferro@users.noreply.github.com>
2023-01-16 13:49:33 -0300

    pronto

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function vermelho(){
+    let img=document.getElementById('img')
+    img.src="imagens/vermelho.png"
+}
+
+function amarelo(){
+    img.src="imagens/amarelo.png"
+}
+
+function verde(){
+    img.src="imagens/verde.png"
+}
+
